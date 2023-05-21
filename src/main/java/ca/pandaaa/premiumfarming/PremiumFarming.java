9db0c198f3412b9f8f18b53e1b9d753d100341df
PandaaaPlays <etienne.toupin03@gmail.com>
2023-05-21 15:24:56 -0400

    v1.0.0 Plugin setup
    Plugin setup (main and Plugin.yml)

--- src/main/java/ca/pandaaa/premiumfarming/PremiumFarming.java
+++ src/main/java/ca/pandaaa/premiumfarming/PremiumFarming.java
@@ -1,17 +1,23 @@
 package ca.pandaaa.premiumfarming;
 
+import ca.pandaaa.utils.Metrics;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PremiumFarming extends JavaPlugin {
 
+    private static PremiumFarming plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        int pluginID = 18531;
+        Metrics metrics = new Metrics(this, pluginID);
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "   &2_____  &a_____ "));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &2|  _  |&a|   __|    &2Prem&aium&8Farm&7ing"));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &2|   __|&a|   __|    &7Version " + getDescription().getVersion()));
+        getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "  &2|__|   &a|__|         &7by &8Pa&7nd&5aaa"));
+        getServer().getConsoleSender().sendMessage("");
     }
 }
